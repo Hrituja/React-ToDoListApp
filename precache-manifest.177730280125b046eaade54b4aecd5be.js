ab9db761c46caea0f6e5529be76a20b4d0d05682
Hrituja <hrkhatavkar@gmail.com>
2020-06-17 00:57:36 +0530

    Updates

---
+++ precache-manifest.177730280125b046eaade54b4aecd5be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cd0c01d86fae62da54ef21d112f7b2",
+    "url": "/React-TodoListApp/index.html"
+  },
+  {
+    "revision": "c1402a47a7b6c0d42201",
+    "url": "/React-TodoListApp/static/css/main.627991ea.chunk.css"
+  },
+  {
+    "revision": "ba4d6dbb438069aca086",
+    "url": "/React-TodoListApp/static/js/2.362019ef.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/React-TodoListApp/static/js/2.362019ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1402a47a7b6c0d42201",
+    "url": "/React-TodoListApp/static/js/main.e7fe0185.chunk.js"
+  },
+  {
+    "revision": "e8efacdc270b5c4ee498",
+    "url": "/React-TodoListApp/static/js/runtime-main.b7b6aae9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-TodoListApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
